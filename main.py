9ec37687f561d91775bb271ef8e7fb5b5678d867
Laazaarus <erykgrabarczyk02@gmail.com>
2019-12-03 23:01:47 +0100

    Basic test code with some drawings of block //Game_area

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame as pg
-###
+######################################################################
+#START##START##START##START##START##START##START##START##START##START#
+######################################################################
 pg.init()
 ###
 scr_width = 500
 scr_height = 500
 wnd = pg.display.set_mode((scr_width, scr_height))
 pg.display.set_caption("PONG by Laazaarus")
-###
+############################################################
+#VARIABLES##CLASSES##VARIABLES##CLASSES##VARIABLES##CLASSES#
+############################################################
+class Tile():
+    def __init__(self, pos_x, pos_y, dim, color):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dim = dim
+        self.color = color
+        self.isDefault = True
+    
+    def drawRectangle(self, wnd):
+        pg.draw.rect(wnd, self.color, (self.pos_x, self.pos_y, self.dim, self.dim))
 
-######
-#Loop#
-######
+white = (255, 255, 255)
+green = (0, 255, 0)
+
+game_area = [Tile(0, 50 ,60 , white), Tile(20, 50 ,60 , green), Tile(40, 50 ,60 , white),
+             Tile(0, 70 ,60 , green), Tile(20, 70 ,60 , white), Tile(40, 70 ,60 , green),
+             Tile(0, 90 ,60 , white), Tile(20, 90 ,60 , green), Tile(40, 90 ,60 , white),]
+
+########################################################################
+#Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop#
+########################################################################
 run = True
 while run:
     
@@ -21,3 +42,8 @@ while run:
             isClicked = pg.mouse.get_pressed()
         if event.type == pg.MOUSEBUTTONUP:
             isClicked = pg.mouse.get_pressed()
+
+        for tile in game_area:
+            tile.drawRectangle(wnd)
+        
+        pg.display.update()
