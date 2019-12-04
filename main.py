048ef48af4861dbb7b8e54e03f92ace0233233cb
Laazaarus <erykgrabarczyk02@gmail.com>
2019-12-04 14:30:21 +0100

    Game area is now calculated

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ pg.display.set_caption("PONG by Laazaarus")
 #VARIABLES##CLASSES##VARIABLES##CLASSES##VARIABLES##CLASSES#
 ############################################################
 class Tile():
-    def __init__(self, pos_x, pos_y, dim, color):
+    def __init__(self, pos_x, pos_y, color):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.dim = dim
         self.color = color
         self.isDefault = True
+        self.dim = 120
     
     def drawRectangle(self, wnd):
         pg.draw.rect(wnd, self.color, (self.pos_x, self.pos_y, self.dim, self.dim))
@@ -25,13 +25,19 @@ class Tile():
 white = (255, 255, 255)
 green = (0, 255, 0)
 
-game_area = [Tile(0, 50 ,60 , white), Tile(20, 50 ,60 , green), Tile(40, 50 ,60 , white),
-             Tile(0, 70 ,60 , green), Tile(20, 70 ,60 , white), Tile(40, 70 ,60 , green),
-             Tile(0, 90 ,60 , white), Tile(20, 90 ,60 , green), Tile(40, 90 ,60 , white),]
+game_area = [Tile(0, 50, white), Tile(130, 50, green), Tile(260, 50, white),
+             Tile(0, 180, green), Tile(130, 180, white), Tile(260, 180, green),
+             Tile(0, 310, white), Tile(130, 310, green), Tile(260, 310, white),]
 
 ########################################################################
 #Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop#
 ########################################################################
+def drawFrame():
+
+    for tile in game_area:
+            tile.drawRectangle(wnd)
+    
+    pg.display.update()
 run = True
 while run:
     
@@ -42,8 +48,7 @@ while run:
             isClicked = pg.mouse.get_pressed()
         if event.type == pg.MOUSEBUTTONUP:
             isClicked = pg.mouse.get_pressed()
+##############################################
+        drawFrame()
+pg.quit()
 
-        for tile in game_area:
-            tile.drawRectangle(wnd)
-        
-        pg.display.update()
