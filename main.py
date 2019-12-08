309f2832ec73626eed9b5d8d1ef65509851e31ba
Laazaarus <erykgrabarczyk02@gmail.com>
2019-12-08 16:53:56 +0100

    Added win-screen +fixed win-condtion

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ tile_o = pg.image.load("sprites/o.bmp")
 tile_x = pg.image.load("sprites/x.bmp")
 menu_o = pg.image.load("sprites/osmall.bmp")
 menu_x = pg.image.load("sprites/xsmall.bmp")
+winscreen_o = pg.image.load("sprites/win_o.png")
+winscreen_x = pg.image.load("sprites/win_x.png")
 ###
 class Tile():
     def __init__(self, pos_x, pos_y, value):
@@ -43,13 +45,16 @@ class Tile():
         else:
             if self.isX:
                 wnd.blit(tile_x, (self.pos_x, self.pos_y))
+                self.value = 666
             else:
                 wnd.blit(tile_o, (self.pos_x, self.pos_y))
-            self.value = 999
+                self.value = 999
 
 game_area = [Tile(20, 70, 1), Tile(150, 70, 2), Tile(280, 70, 3),
              Tile(20, 200, 4), Tile(150, 200, 5), Tile(280, 200, 6),
              Tile(20, 330, 7), Tile(150, 330, 8), Tile(280, 330, 9)]
+
+whoWins = ''
 
 #Define who starts the game
 if random.randint(0,1) == 0:
@@ -63,10 +68,16 @@ else:
 #Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop##Loop#
 ########################################################################
 def drawFrame():
-    pg.Surface.fill(wnd, (100, 100, 100))
-    for tile in game_area:
-            tile.drawTile(wnd)
-    
+    if whoWins == '':
+        pg.Surface.fill(wnd, (100, 100, 100))
+        for tile in game_area:
+                tile.drawTile(wnd)
+        
+    elif whoWins == 'O':
+            wnd.blit(winscreen_o, (0, 0))
+    elif whoWins == 'X':
+            wnd.blit(winscreen_x, (0, 0))
+
     pg.display.update()
 #####################################
 run = True
@@ -88,23 +99,24 @@ while run:
         ############################################################
         for tile in game_area:
 
-            if tile.isDefault:
-                if ((mousePos[0] > tile.pos_x) and (mousePos[0] < tile.pos_x + tile.dim) and
-                    (mousePos[1] > tile.pos_y) and (mousePos[1] < tile.pos_y +tile.dim)):
+            if whoWins == '':
+                if tile.isDefault:
+                    if ((mousePos[0] > tile.pos_x) and (mousePos[0] < tile.pos_x + tile.dim) and
+                        (mousePos[1] > tile.pos_y) and (mousePos[1] < tile.pos_y +tile.dim)):
 
-                    if isClicked[0] == 1:
-                        if xTurn:
-                            tile.isX = True
-                            xTurn = False
+                        if isClicked[0] == 1:
+                            if xTurn:
+                                tile.isX = True
+                                xTurn = False
+                            else:
+                                tile.isX = False
+                                xTurn = True
+
+                            tile.isDefault = False
                         else:
-                            tile.isX = False
-                            xTurn = True
-
-                        tile.isDefault = False
+                            tile.hoverOver = True
                     else:
-                        tile.hoverOver = True
-                else:
-                    tile.hoverOver = False
+                        tile.hoverOver = False
 
 
         #######
@@ -114,7 +126,7 @@ while run:
         #######
 
             #Horizontal
-        if ((game_area[0].value == game_area[1].value) and (game_area[1].value == game_area[2].value)  or
+        if  ((game_area[0].value == game_area[1].value) and (game_area[1].value == game_area[2].value)  or
             ((game_area[3].value == game_area[4].value) and (game_area[4].value == game_area[5].value)) or
             ((game_area[6].value == game_area[7].value) and (game_area[7].value == game_area[8].value)) or
             #Vertical
@@ -126,9 +138,9 @@ while run:
             ((game_area[2].value == game_area[4].value) and (game_area[4].value == game_area[6].value))):
 
             if xTurn == True:
-                print("Wygrywa gracz O")
+                whoWins = 'O'
             else:
-                print("Wygrywa gracz X")    
+                whoWins = 'X'
 
         drawFrame()
 
